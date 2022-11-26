b3c30fe7969eeddfa5e24ae1f81911b857495315
bushuev-alex <bushuev-alex@mail.ru>
2022-11-26 20:56:23 +0700

    initial commit

---
+++ Static Code Analyzer/task/analyzer/check_methods.py
@@ -0,0 +1,145 @@
+import re
+import ast
+
+
+class LineByLineChecker:
+
+    def __init__(self):
+        self.errors = []
+        self.blank_line_count = 0
+        self.codes = {'S001': 'Too long',
+                      'S002': 'Indentation is not a multiple of four',
+                      'S003': 'Unnecessary semicolon after a statement',
+                      'S004': 'Less than two spaces before inline comments',
+                      'S005': 'TODO found',
+                      'S006': 'More than two blank lines preceding a code line',
+                      'S007': "Too many spaces after '{}'",
+                      'S008': "Class name '{}' should be written in CamelCase",
+                      'S009': 'Function name {} should be written in snake_case',
+                      'S010': "Argument name {} should be written in snake_case",
+                      'S011': "Variable {} should be written in snake_case",
+                      'S012': "Default argument value is mutable"}
+
+    def error_s001(self, line_no: int, line: str):  # 'Too long' e.c. >= 80
+        if len(line) >= 80:
+            self.errors.append([line_no,
+                                'S001',
+                                self.path,
+                                f"{self.path}: Line {line_no}: S001 {self.codes['S001']}"])
+
+    def error_s002(self, line_no: int, line: str):  # Indentation is not a multiple of four
+        result = re.match("^( *)", line)
+        if len(result.group()) % 4 != 0:  # or """if len(re.match('^ *', line)[0]) % 4 != 0:"""
+            self.errors.append([line_no,
+                                'S002',
+                                self.path,
+                                f"{self.path}: Line {line_no}: S002 {self.codes['S002']}"])
+
+    def error_s003(self, line_no: int, line: str):  # Unnecessary semicolon after a statement
+        if re.match(".*\(.*\);.*", line) or \
+                re.match(" *[A-Za-z]*;", line):
+            # print(line.strip(), '-------', res)
+            self.errors.append([line_no,
+                                'S003',
+                                self.path,
+                                f"{self.path}: Line {line_no}: S003 {self.codes['S003']}"])
+
+    def error_s004(self, line_no: int, line: str):  # Less than two spaces before inline comments
+        if '#' in line:
+            if re.match("^#.*", line):
+                pass
+            elif re.match('.*( ){2,}#.*', line) is None and '#' in line:
+                self.errors.append([line_no,
+                                    'S004',
+                                    self.path,
+                                    f"{self.path}: Line {line_no}: S004 {self.codes['S004']}"])
+
+    def error_s005(self, line_no: int, line: str):  # TO_DO found (in comments only and case-insensitive)
+        if re.match('.*#.*TODO.*', line, re.IGNORECASE):
+            self.errors.append([line_no,
+                                'S005',
+                                self.path,
+                                f"{self.path}: Line {line_no}: S005 {self.codes['S005']}"])
+
+    def error_s006(self, line_no: int, line: str):  # More than two blank lines preceding a code line
+        if line.strip() == '':
+            self.blank_line_count += 1
+            if self.blank_line_count > 2:
+                self.errors.append([line_no + 1,
+                                    'S006',
+                                    self.path,
+                                    f"{self.path}: Line {line_no + 1}: S006 {self.codes['S006']}"])
+                self.blank_line_count = 0
+
+    def error_s007(self, line_no: int, line: str):  # Too many spaces after construction_name (def or class)
+        if re.match(".*(class|def) {2,}.*", line):
+            beginning_line = re.match(".*(class|def) {2,}", line)[0]
+            if 'class' in beginning_line:
+                name = line[len(beginning_line):-2]
+            elif 'def' in beginning_line:
+                name = line[len(beginning_line):-4]
+            self.errors.append([line_no,
+                                'S007',
+                                self.path,
+                                f"{self.path}: Line {line_no}: S007 {self.codes['S007'].format(name)}"])
+
+    def error_s008(self, line_no: int, line: str):  # Class name {} should be written in CamelCase
+        if 'class' in line:
+            if not re.match(".* ?class [A-Z][a-zA-Z]*(\\([A-Z][a-zA-Z]*\\))?:", line):
+                if re.match(".*class [a-z]+", line):
+                    beginning_line = re.match(".*class +[A-Z]?", line)[0]
+                    class_name = line[len(beginning_line):-2]
+                    self.errors.append([line_no,
+                                        'S008',
+                                        self.path,
+                                        f"{self.path}: Line {line_no}: S008 {self.codes['S008'].format(class_name)}"])
+
+    def error_s009(self, line_no: int, line: str):  # Function name {} should be written in snake_case
+        if 'def' in line:
+            if not re.match(".*def _{,2}?[a-z]*_?[a-z0-9]*_{,2}?\\(.*\\):$", line):
+                if re.match(".*def +[A-Z]", line):
+                    beginning_line = re.match(".*def +[a-z]?", line)[0]
+                    def_name = line[len(beginning_line):-4]
+                    self.errors.append([line_no,
+                                        'S009',
+                                        self.path,
+                                        f"{self.path}: Line {line_no}: S009 {self.codes['S009'].format(def_name)}"])
+
+
+class ASTChecker:
+
+    def check_node_in_snake_case(self, obj):
+        func = obj.__dict__
+        lineno = func['lineno']
+        arg_name = func['arg']
+        if not re.match("^[a-z0-9_]*_?[a-z0-9]*$", arg_name):
+            self.errors.append([lineno,
+                                'S010',
+                                self.path,
+                                f"{self.path.lower()}: Line {lineno}: S010 {self.codes['S010'].format(arg_name)}"])
+
+    def check_variable_in_snake_case(self, obj):
+        if isinstance(obj, ast.Assign):
+            for target in obj.targets:
+                var_dict = target.__dict__
+                try:
+                    var_name = var_dict['id']
+                    lineno = var_dict['lineno']
+                except KeyError:
+                    var_name = var_dict['value'].id + '.' + var_dict['attr']
+                    lineno = var_dict['lineno']
+                if not re.match("^[a-z0-9_]*\.?_?[a-z0-9]*$", var_name):
+                    self.errors.append(
+                        [lineno,
+                         'S011',
+                         self.path,
+                         f"{self.path}: Line {lineno}: S011 {self.codes['S011'].format(var_name)}"])
+
+    def check_default_argument_mutable(self, default):
+        if not isinstance(default, ast.Constant):
+            default_dict = default.__dict__
+            lineno = default_dict['lineno']
+            self.errors.append([lineno,
+                                'S012',
+                                self.path,
+                                f"{self.path}: Line {lineno}: S012 {self.codes['S012']}"])
---
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -0,0 +1,84 @@
+import re
+import os
+import argparse
+import ast
+from check_methods import LineByLineChecker, ASTChecker
+
+
+class Checker(LineByLineChecker, ASTChecker):
+
+    def __init__(self, path_: str):
+        super().__init__()
+        self.path = path_
+
+    def blank_line(self, m_line: str):
+        if m_line.strip() != '':
+            self.blank_line_count = 0
+
+    def check_line_by_line(self, line_no: int, line: str):
+        self.blank_line(line)
+        self.error_s001(line_no, line)  # Too long
+        self.error_s002(line_no, line)  # Indentation is not a multiple of four
+        self.error_s003(line_no, line)  # Unnecessary semicolon after a statement
+        self.error_s004(line_no, line)  # Less than two spaces before inline comments
+        self.error_s005(line_no, line)  # TO_DO found (in comments only and case-insensitive)
+        self.error_s006(line_no, line)  # More than two blank lines preceding a code line
+        self.error_s007(line_no, line)  # Too many spaces after construction_name (def or class)
+        self.error_s008(line_no, line)  # Class name {} should be written in CamelCase
+        self.error_s009(line_no, line)  # Function name {} should be written in snake_case
+
+    def check_ast_nodes(self, file):
+        text = file.read()
+        tree = ast.parse(text)
+        nodes = ast.walk(tree)
+        for node in nodes:
+            if isinstance(node, ast.FunctionDef):
+                for obj in node.args.args:  # Argument name {} should be written in snake_case
+                    self.check_node_in_snake_case(obj)
+                for obj in node.body:  # Variable {} should be written in snake_case
+                    self.check_variable_in_snake_case(obj)
+                for default in node.args.defaults:  # The default argument value is mutable
+                    self.check_default_argument_mutable(default)
+
+    def check_file(self):
+        if re.match('.*\\.py$', self.path):
+            # Line-by-line checkers
+            with open(self.path, 'r', encoding="UTF-8") as file:
+                for n_line, line in enumerate(file, start=1):
+                    self.check_line_by_line(n_line, line)
+            # AST node checkers
+            with open(self.path, 'r', encoding="UTF-8") as file:
+                self.check_ast_nodes(file)
+
+    def check_dir(self):
+        file_list = os.listdir(self.path)
+        file_list.sort()
+        for file in file_list:
+            path = self.path
+            self.path = self.path + f'{os.sep}' + file
+            self.check_file()
+            self.path = path
+
+    def select_dir_or_file(self):
+        if os.path.isdir(self.path):
+            return "dir"
+        elif os.path.isfile(self.path):
+            return "file"
+
+    def print_errors(self):
+        self.errors.sort(key=lambda element: (element[2], element[0], element[1]))
+        for error in self.errors:
+            print(error[3])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", default='.')
+    args = parser.parse_args()
+    path = args.path
+    my_checker = Checker(path)
+    if my_checker.select_dir_or_file() == 'file':
+        my_checker.check_file()
+    elif my_checker.select_dir_or_file() == 'dir':
+        my_checker.check_dir()
+    my_checker.print_errors()
