01c3d4e905a8787e077f9d5197205593777885f6
bushuev-alex <bushuev-alex@mail.ru>
2023-09-02 23:27:27 +0700

    1. replace all try-except blocks in check_variable_in_snake_case() with if-else and __dict__.get(some_key)

--- Static Code Analyzer/task/analyzer/check_methods.py
+++ Static Code Analyzer/task/analyzer/check_methods.py
@@ -110,21 +110,18 @@ class ASTChecker:
     def check_variable_in_snake_case(self, obj):
         if isinstance(obj, ast.Assign):
             for target in obj.targets:
-                # print(target)
                 var_dict = target.__dict__
-                # print(var_dict)
-                try:
-                    var_name = var_dict['id']
-                    lineno = var_dict['lineno']
-                except KeyError:
-                    # print(var_dict.get("value").__dict__)
-                    try:
-                        var_name = var_dict['value'].id + '.' + var_dict['attr']
-                        lineno = var_dict['lineno']
-                    except AttributeError:
-                        var_name = var_dict['value'].value.id + '.' + var_dict['value'].attr
-                        lineno = var_dict['value'].lineno
-                        # print(var_dict['value'].value.id + '.' + var_dict['value'].attr)
+                var_name = var_dict.get('id')
+                lineno = var_dict.get('lineno')
+                if not (var_name and lineno):
+                    var_name_id = var_dict.get('value').__dict__.get('id')
+                    var_name_attr = var_dict.get('attr')
+                    lineno = var_dict.get('lineno')
+                    if not (var_name_id and var_name_attr and lineno):
+                        var_name_id = var_dict.get('value').__dict__.get('value').__dict__.get('id')
+                        var_name_attr = var_dict.get('value').__dict__.get('attr')
+                        lineno = var_dict.get('value').__dict__.get('lineno')
+                    var_name = '.'.join((var_name_id, var_name_attr))
                 if not re.match("^[a-z0-9_]*\.?_?[a-z0-9]*$", var_name):
                     self.errors.append(
                         [lineno,
